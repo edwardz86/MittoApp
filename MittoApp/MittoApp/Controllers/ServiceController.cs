045ae8e131aa8cccb02e06b7e69f61e6c9cef1bb
stolarczykt <tomaszstolarczyk86@gmail.com>
2016-09-16 12:43:31 +0200

    SendSMS API

--- MittoApp/MittoApp/Controllers/ServiceController.cs
+++ MittoApp/MittoApp/Controllers/ServiceController.cs
@@ -2,17 +2,19 @@
 using MittoApp.Migrations;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using static MittoApp.Utils.Enums;
 
 namespace MittoApp.Controllers
 {
     public class ServiceController : ApiController
     {
         private MittoContext db = new MittoContext();
-
+        
         [HttpGet]
         [Route("countries{x:regex(.[A-Z])}")]
         public IEnumerable<CountryDTO> GetCountries()
@@ -29,6 +31,32 @@ namespace MittoApp.Controllers
             return countries.ToList();            
         }
 
+        [HttpPost]
+        [Route("sms/send{x:regex(.[A-Z])}")]
+        public State SendSMS(string from, string to, string text)
+        {
+            Countries country = db.Countries.FirstOrDefault();
+
+            if(country == null)
+            {
+                return State.Failed;
+            }
+   
+            db.Messages.Add(getMessage(from, to, text, country));
+
+            try
+            {
+                db.SaveChanges();
+                
+            }
+            catch (DbUpdateException)
+            {
+                throw;                
+            }
+            
+            return State.Success;
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -37,5 +65,22 @@ namespace MittoApp.Controllers
             }
             base.Dispose(disposing);
         }
+
+        private Messages getMessage(string from, string to, string text, Countries country)
+        {
+            Messages message = new Messages()
+            {
+                Id = Guid.NewGuid(),
+                Sender = from,
+                Receiver = to,
+                Text = text,
+                SendDate = DateTime.Now,
+                State = 0,
+                Country = country
+            };
+
+            return message;
+        }
+
     }
 }
